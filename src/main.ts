244e1b9f3511122e1cc8f9ce26b0ac45ea641954
Norma Bojorquez <norma_bojorquez26@hotmail.com>
2024-02-03 18:13:19 -0700

    Menu Iniciado
    Menu del juego con diseño sencillo, agregar opciones

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,18 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//acomoar el cubo y la cuadricula en la escena
+// Posicionar el cubo y la cuadrícula en la escena
 scene.add(myCube);
-//rotar en x
 myCube.rotation.x = Math.PI / 4;
 scene.add(myCuadricula);
 
 // Configurar la cámara
 camera.position.z = 5;
 
-
-// Configurar luz
+// Configurar luz ambiental
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
@@ -29,7 +26,6 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
 
-
 const animate = function () {
   requestAnimationFrame(animate);
 
@@ -37,3 +33,16 @@ const animate = function () {
 };
 
 animate();
+
+// Obtener referencia al botón "Jugar"
+const playButton = document.getElementById('playButton');
+
+// Obtener referencia al texto de bienvenida
+const infoText = document.getElementById('info');
+
+// Escuchar el evento 'click' en el botón "Jugar"
+playButton!.addEventListener('click', () => {
+  // Ocultar el texto de bienvenida y el botón "Jugar"
+  infoText!.style.display = 'none';
+  playButton!.style.display = 'none';
+});
